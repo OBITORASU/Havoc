190947a92f5cfd9e5a00b495dcabdba85c46901b
Souhit Dey <obitogod@protonmail.com>
2026-02-12 02:52:29 +0530

    Fix Client UI crash when removing agent in Table View
    There is a bug which causes the client to crash if the user tries to remove an agent from the table view.

--- client/src/UserInterface/Widgets/SessionGraph.cc
+++ client/src/UserInterface/Widgets/SessionGraph.cc
@@ -89,10 +89,10 @@ void GraphWidget::GraphNodeRemove( SessionItem Session )
         {
             GraphScene->removeItem( NodeList[ i ]->Node->NodeEdge );
             GraphScene->removeItem( NodeList[ i ]->Node );
-
-            NodeList.erase( NodeList.begin() + i );
+            
             MainNode->Node->removeChild( NodeList[ i ]->Node );
-
+            NodeList.erase( NodeList.begin() + i );
+            
             /* delete NodeList[ i ]->Node->NodeEdge;
             delete NodeList[ i ]->Node;
             delete NodeList[ i ]; */
